79504f6b33158b5ac00f85830bcd07151257043b
Hongwei-W <hongwei_charles_wang@outlook.com>
2022-08-27 23:02:30 -0600

    *MDF [Intro] edit intro text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,19 +11,20 @@ const Intro = () => {
       </div>
       <div className="intro section">
         <span>
-          <span className="t2">Hongwei Wang</span> (pronoun: he/him) is a fourth
-          year Computing Science student at University of Alberta. He is also a
-          current Research Assistant in Dr. Xing-dong Yang's (SFU) group and in
-          Dr. Carrie Demmans Epp's (UAlberta) Group. He has special interest in
-          Human Computer Interaction (HCI) and accessibility tools.
+          <span className="t2">Hongwei Wang</span> (pronoun: he/him) is a
+          fourth-year Computing Science student at the University of Alberta. He
+          is also a current Research Assistant in Dr. Xing-dong Yang's (Simon
+          Fraser University - ixLab) group and Dr. Carrie Demmans Epp's
+          (University of Alberta - EdTeKLA) Group. He has a special interest in
+          Human-Computer Interaction (HCI) and accessibility tools.
           <br />
           <br />
           Other than Computing Science, he is interested in Hotel Management and
-          Psychology. He loves to know people from hotel industry. His favorite
-          brand is Hyatt but he only has elite status in Marriott.
+          Psychology. He loves to know people from the hotel industry. His
+          favorite brand is Hyatt but he only has an elite status in Marriott.
           <br />
           <br />
-          He is also a newbie photographer, he shoot on Fujifilm XS-10.
+          He is also a newbie photographer, he shoots on Fujifilm XS-10.
           <br />
           <br />
           You can reach him via email `hongwei2[at]ualberta[dot]ca`, more
